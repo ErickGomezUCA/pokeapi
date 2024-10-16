f8d016fa9795ae82a41ff8bc0371394f13e218bf
ErickGomezUCA <00300723@uca.edu.sv>
2024-10-15 22:50:34 -0600

    Things

--- server/server.js
+++ server/server.js
@@ -1,24 +1,31 @@
 import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
+import { fileURLToPath } from 'url';
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(bodyParse.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.render('main');
 });
 
-app.post('/pokemon', (req, res) => {
-  res.render('pokemon');
+app.get('/create', (req, res) => {
+  res.render('create');
 });
 
-app.listen(3000, () => {
-  console.log(`Server is running on http://localhost:${3000}`);
+app.post('/create', (req, res) => {});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
